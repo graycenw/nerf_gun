a76974b3d73034cb19d7d75eedaf9e676a4d0f96
kristen-schneider <kristen.schneider@colorado.edu>
2020-01-29 15:49:57 -0700

    adding some small code to the design.sh file. not functionable yet. need to discover columns

--- create_design_file.sh
+++ create_design_file.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# print a header
 HEADER = "group, replicate, fastq_1, fastq_2, anitbody, control, fastq1_url, fastq2_url"
 echo $HEADER > NRF1_design.csv
 
-# group 
-# replicate
-# fastq_1
-# fastq_2
-# anitbody
-# control
-# fastq1_url
-# fastq2_url
+###NOTE: these are the mapped columsn to each of the data we need
+GROUP =  
+REPLICATE = 
+FASTQ_1 = 
+FASTQ_2 = 
+ANTIBODY = 
+CONTROL = 
+FASTQ1_URL = 
+FASTQ2_URL = 
 
+awk -F '\t' '{print $GROUP, $REPLICATE, $FASTQ_1, $FASTQ_2, $ANTIBODY, $CONTROL, $FASTQ1_URL, $FASTQ2_URL}' metadata.tsv > NRF1_design.csv
 
