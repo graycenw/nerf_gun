f7104cb5f08a537fcdab000f7b1e767d61bd5efe
kristen-schneider <kristen.schneider@colorado.edu>
2020-01-29 15:43:49 -0700

    adding a .sh file to start a script to create the design file

---
+++ create_design_file.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+HEADER = "group, replicate, fastq_1, fastq_2, anitbody, control, fastq1_url, fastq2_url"
+echo $HEADER > NRF1_design.csv
+
+# group 
+# replicate
+# fastq_1
+# fastq_2
+# anitbody
+# control
+# fastq1_url
+# fastq2_url
+
+
